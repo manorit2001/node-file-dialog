274056bdd1ed89886f597d2f639ed4aae31fbd26
manorit chawdhry <manorit2001@gmail.com>
2020-12-02 18:22:54 +0530

    index.js windows compatibility done
    - pyinstaller build for windows still remain

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const {exec} = require('child_process');
+const path = require('path');
 const root = __dirname;
 
 function askdialog(config) {
-  var cmd = '/python/dist/dialog '
-  if (config.type == 'directory')
+  var cmd = path.join('python', 'dist')
+  var filename = 'dialog'
+  if (process.arch === 'x86') filename += '-x86'
+  if (process.platform === 'linux') cmd = path.join(cmd, filename)
+  if (process.platform === 'win32') cmd = path.join(cmd, filename + '.exe')
+  if (config.type === 'directory')
   cmd += ' -d';
-  else if (config.type == 'saveFile')
+  else if (config.type === 'saveFile')
   cmd += ' -s';
-  else if (config.type == 'openFile')
+  else if (config.type === 'openFile')
   cmd += ' -o';
-  else if (config.type == 'openMultipleFiles')
+  else if (config.type === 'openMultipleFiles')
   cmd += ' -f';
   var promise = new Promise((resolve, reject) => {
-    exec(root + cmd, (error, stdout, stderr) => {
+    exec(path.join(root, cmd), (error, stdout, stderr) => {
       if (error) {
         reject(new Error(error.message));
       } else if (stderr) {
