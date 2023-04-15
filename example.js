e7c508145ba09c0629ac22ef95153be6b5200c8c
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-04-15 18:04:49 +0200

    update with new config

--- example.js
+++ example.js
@@ -1,5 +1,15 @@
-const dialog = require('node-file-dialog')
+const dialog = require('./index').dialog;
 const config = {
-  type: 'directory'
+  type: 'open-file',
+  extra: {
+    ext: ".mp4",
+    types: {
+      "mp4 files": '.mp4',
+      "webm files": ".webm",
+      "mov files": ".mov"
+    },
+    startdir: "E:\\development\\node-file-dialog-fork",
+    title: 'selection test'
+  }
 };
 dialog(config).then(dir => console.log(dir)).catch(err => console.log(err))
