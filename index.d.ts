e88f145fddd17eb90ba66ac1272faaf13b611c0d
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-23 16:19:08 +0200

    reworking: typescript

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace dialogs {
-    function dialog(config: {type: 'directory' | 'save-file' | 'open-file' | 'open-files', extra: {ext: string, types: Object<string, string>, startdir: string, startfile: string, title: string}}): Promise<string[]>;
-}
-
-export = dialogs
